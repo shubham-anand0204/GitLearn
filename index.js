0eb1f6271fe174b2a9a2dabb545b14b52c0d4cc4
shubham-anand0204 <shubham1382.be22@chitkarauniversity.edu.in>
2025-08-29 22:50:36 +0530

    Second Change

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const age = 24
 
-const name = "Shubham Anand"
+const name = " Mr Shubham Anand"
 const dob = '01/01/2004'
 
 function sayName(){
-    console.log('My name is ' + name)
+    console.log('My name is ' + name) 
+}
+
+
+function isAbove18(){
+    return age >=18;
 }
